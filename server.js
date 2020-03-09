7c8d54813079f3d513038bc06dc91c4de346ae60
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-08 22:08:51 -0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
+
+app.set("port", process.env.PORT || port);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
+});
+
+app.get("/*", function(request, response) {
+  // TODO: if the request is just /, serve the homepage
+  // if it's /<something>, call redirect
+  response.sendFile(path.join(__dirname, "/build/index.html"));
+});
+
+// app.get("/encode/*", function(request, response) {
+//   // TODO: forward this request to /e/
+// });
