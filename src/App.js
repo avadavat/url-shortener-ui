12026841304b081092532508f9d218425b1acc98
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-08 22:26:06 -0400

    fully working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
 import "./App.css";
-import { AppBody, Footer } from "./view";
 
 /**
  * App is the root wrapper around the application.
  */
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AppBody />
-        <Footer />
-      </header>
-    </div>
-  );
+  return <div className="App">donk</div>;
 }
 
 export default App;
