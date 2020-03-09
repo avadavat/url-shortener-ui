b5279694ea45282c38b04b38b8bc49416a945ff0
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-08 22:13:40 -0400

    Add initial files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import { AppBody, Footer } from "./view";
+
+/**
+ * App is the root wrapper around the application.
+ */
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppBody />
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
