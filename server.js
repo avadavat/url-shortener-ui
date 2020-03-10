122fb5063ac9b4262763fb7fd7377c6b93aad9d0
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-09 22:03:50 -0400

    Working encoding

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const axios = require("axios");
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8080;
+
+const serviceHost = "http://wittle-wink.herokuapp.com";
+const redirectEndpoint = serviceHost + "/r";
+const encodeEndpoint = serviceHost + "/e";
 
 app.set("port", process.env.PORT || port);
 
@@ -12,16 +17,29 @@ app.listen(app.get("port"), function() {
 });
 
 app.get("/", function(request, response) {
-  // TODO: if the request is just /, serve the homepage
-  // if it's /<something>, call redirect
+  // Serve the homepage on root requests.
   response.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 app.get("/:shortLink", function(request, response) {
-  response.send("redirect");
+  // Redirect /<shortLink> requests to the url shortening service.
+  response.redirect(redirectEndpoint + request.url);
 });
 
-app.get("/encode/:longLink", function(request, response) {
-  // TODO: forward this request to /e/
-  response.send("somehash");
+app.get("/encode/*", function(request, response) {
+  // Proxy /encode/<someURL> to the url shortening service.
+  var requestUrl = encodeEndpoint + request.url.substring("/encode".length);
+
+  axios
+    .get(requestUrl)
+    .then(function(res) {
+      response.send(res.data);
+    })
+    .catch(function() {
+      response.sendStatus(500);
+    });
+});
+
+app.get("/decode/:shortLink", function(request, response) {
+  // TODO: route to /d/
 });
--- src/view/InputArea.tsx
+++ src/view/InputArea.tsx
@@ -1,33 +1,38 @@
 import React from "react";
+import { InputBox } from "./InputBox";
+
+const axios = require("axios");
+
+const thisHost = window.location.origin;
 
 const inputAreaStyle: React.CSSProperties = {
   fontSize: 18
 };
 
 export const InputArea = React.memo(function InputArea() {
+  const [query, setQuery] = React.useState<string>("");
   const [message, setMessage] = React.useState<string>("");
 
+  const onQueryChange = (newQuery: string) => {
+    setQuery(newQuery);
+  };
+
   const onClick = () => {
-    // todo: implement onClick
-    // setMessage("clicky");
-
-    var xhr = new XMLHttpRequest();
-
-    // get a callback when the server responds
-    xhr.addEventListener("load", () => {
-      // update the state of the component with the result here
-      console.log(xhr.responseText);
-      setMessage(xhr.responseText);
-    });
-    // open the request with the verb and the url
-    xhr.open("GET", "/encode/something");
-    // send the request
-    xhr.send();
+    axios
+      .get("/encode/" + query)
+      .then(function(res: any) {
+        const shortLink: string = res.data;
+        const fullLink = `${thisHost}/${shortLink}`;
+        setMessage(fullLink);
+      })
+      .catch(function() {
+        setMessage("error. text dan for help");
+      });
   };
 
   return (
     <div className="rowC">
-      <input style={inputAreaStyle} />
+      <InputBox query={query} onQueryChange={onQueryChange} />
       <button onClick={onClick}>Encode</button>
       <div>{message}</div>
     </div>
