38904aa9c9537b0328203e98066f7dfc57665362
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-09 21:24:55 -0400

    Add InputBox from truth-fiddle

---
+++ src/view/InputBox.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const inputBoxStyle: React.CSSProperties = {
+  borderRadius: 24,
+  paddingLeft: 8,
+  paddingRight: 8,
+  fontSize: 24,
+  textAlign: "center",
+  verticalAlign: "middle",
+  outline: "none"
+};
+
+interface InputBoxProps {
+  query: string;
+  onQueryChange: (query: string) => void;
+}
+
+/**
+ * InputBox is where the user can enter their boolean expression.
+ */
+export const InputBox = React.memo(function InputBox(props: InputBoxProps) {
+  const { query } = props;
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.onQueryChange(event.target.value);
+  };
+
+  return <input style={inputBoxStyle} onChange={onChange} value={query} />;
+});
